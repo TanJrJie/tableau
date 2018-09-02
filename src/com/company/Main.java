b0dd6209426e7799d83d10b54b75f679a2dbab57
thomas <tanzhijiego@163.com>
2018-09-03 00:19:42 +0800

    初步规划

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("第一步：查询PG库");
+        System.out.println("第二部：获取Tick");
     }
 }
