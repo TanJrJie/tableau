223540d939c82a22d769ffe344aceea2697aa077
thomas <tanzhijiego@163.com>
2018-09-03 00:51:37 +0800

    测试

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("第一步：查询PG库");
         System.out.println("第二部：获取Tick");
+        System.out.println("第二部：获取Tick");
     }
 }
